e669c47552e34884cdf5262759147e808f06f45e
alexnguyen02 <leminhtrang.nguyen@outlook.com>
2023-09-11 12:35:03 -0400

    add Main.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // FizzBuzz.doFizzBuzz(5);
+        // Result: The loop does not get executed; the input i only get "Fizz-Buzz" once
+        String[] strArray = new String[] {"A","B","C"};
+
+        FizzBuzz.main(strArray);
+    }
+}
